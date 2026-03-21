0eb770225cf48d1e7b06a8c446305b23a1fc10f3
Haruto Matsushima <hrt861.dly@gmail.com>
2026-03-21 20:31:06 +0900

    Fix AppShell bg to show blue-grey instead of white

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -2,7 +2,7 @@ import TopNav, { BottomNav } from './Sidebar';
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
+    <div className="min-h-screen flex flex-col bg-[#E3EAF6]">
       <TopNav />
       <div className="flex-1 flex flex-col min-w-0 pb-20 md:pb-0">
         {children}
